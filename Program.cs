8a09c566d51c7ff126c43d0f160cbe6e340b1e6a
Phoenix <110415215+LetsBePhoenix@users.noreply.github.com>
2023-08-17 16:18:23 +0200

    a.v.0.1
    - Feld wird Erzeugt
    - Spieler-wechsel hinzugefügt
    - Garavity hinzugefügt
    - Zeigen und Erneuern des Feldes

--- Program.cs
+++ Program.cs
@@ -1,7 +1,95 @@
 ﻿internal class Program
 {
+    public static int Player = 1;
+    public static int[,] Field = new int[6, 7] { { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 } };
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int ChosenLine;
+        while (true) 
+        {
+            Console.Clear();
+            ShowField();
+            try
+            {
+                Console.Write("Spalte: ");
+                ChosenLine = Convert.ToInt32(Console.ReadLine());
+                ChosenLine--; // -- because line 1 is actually line 0...
+                if (ChosenLine >= 0 && ChosenLine <= 6)
+                {
+                    if (Field[0, ChosenLine] == 0) // Chack if you can place
+                    {
+                        /*
+                        Makes the gravity, whitch takes the Plates down
+                        */
+                        if (Field[1, ChosenLine] == 0)
+                        {
+                            if (Field[2, ChosenLine] == 0)
+                            {
+                                if (Field[3, ChosenLine] == 0)
+                                {
+                                    if (Field[4, ChosenLine] == 0)
+                                    {
+                                        if (Field[5, ChosenLine] == 0)
+                                        {
+                                            Field[5, ChosenLine] = SwitchPlayerAndFiedback();
+                                        }
+                                        else
+                                        {
+                                            Field[4, ChosenLine] = SwitchPlayerAndFiedback();
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Field[3, ChosenLine] = SwitchPlayerAndFiedback();
+                                    }
+                                }
+                                else
+                                {
+                                    Field[2, ChosenLine] = SwitchPlayerAndFiedback();
+                                }
+                            }
+                            else
+                            {
+                                Field[1, ChosenLine] = SwitchPlayerAndFiedback();
+                            }
+                        }
+                        else
+                        {
+                            Field[0, ChosenLine] = SwitchPlayerAndFiedback();
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            { }
+        }
+    }
+    private static int SwitchPlayerAndFiedback()
+    {
+        if (Player == 1)
+        {
+            Player = 2;
+            return 1;
+        }
+        else
+        {
+            Player = 1;
+            return 2;
+        }
+    }
+    private static void RenewField()
+    {
+         Field = new int[6, 7] { { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 } };
+    }
+    private static void ShowField()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                Console.Write(Field[i, j]);
+            }
+            Console.WriteLine();
+        }
     }
 }
