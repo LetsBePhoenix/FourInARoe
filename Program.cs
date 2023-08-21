ad96f49fefc95beb171b7147471e29e55f63284d
Phoenix <110415215+LetsBePhoenix@users.noreply.github.com>
2023-08-21 11:08:15 +0200

    a.v.0.2
    - Funktion zum überprüfen, wer gewinnt hinzugefügt

--- Program.cs
+++ Program.cs
@@ -2,10 +2,12 @@
 {
     public static int Player = 1;
     public static int[,] Field = new int[6, 7] { { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 } };
+    public static int Player_win1 = 0;
+    public static int Player_win2 = 0;
     private static void Main(string[] args)
     {
         int ChosenLine;
-        while (true) 
+        while (true)
         {
             Console.Clear();
             ShowField();
@@ -62,8 +64,88 @@
             }
             catch(Exception ex)
             { }
+            TryWin(Player);
         }
     }
+    private static void TryWin(int player)
+    {
+        bool Winn = false;
+        // Horizontal
+        for (int hight = 0; hight < 6; hight++)
+        {
+            if (Field[hight, 0] == player && Field[hight, 1] == player && Field[hight, 2] == player && Field[hight, 3] == player)
+            {
+                Winn = true;
+            }
+            else if (Field[hight, 1] == player && Field[hight, 2] == player && Field[hight, 3] == player && Field[hight, 4] == player)
+            {
+                Winn = true;
+            }
+            else if (Field[hight, 2] == player && Field[hight, 3] == player && Field[hight, 4] == player && Field[hight, 5] == player)
+            {
+                Winn = true;
+            }
+            else if (Field[hight, 3] == player && Field[hight, 4] == player && Field[hight, 5] == player && Field[hight, 6] == player)
+            {
+                Winn = true;
+            }
+        }
+        // Vertical
+        for (int lenght = 0; lenght < 5; lenght++)
+        {
+            if (Field[0, lenght] == player && Field[1, lenght] == player && Field[2, lenght] == player && Field[3, lenght] == player)
+            {
+                Winn = true;
+            }
+            else if (Field[1, lenght] == player && Field[2, lenght] == player && Field[3, lenght] == player && Field[4, lenght] == player)
+            {
+                Winn = true;
+            }
+            else if (Field[2, lenght] == player && Field[3, lenght] == player && Field[4, lenght] == player && Field[5, lenght] == player)
+            {
+                Winn = true;
+            }
+        }
+        // Diagonal Left to Right
+        for (int a = 0; a < 3; a++)
+        {
+            for (int b = 0; b < 4; b++)
+            {
+                if (Field[a, b] == player && Field[a + 1, b + 1] == player && Field[a + 2, b + 2] == player && Field[a + 3, b + 3] == player)
+                {
+                    Winn = true;
+                }
+            }
+        }
+        // Diagonal Right to Left
+        for (int a = 0; a < 3; a++)
+        {
+            for (int b = 6; b > 2; b--)
+            {
+                if (Field[a, b] == player && Field[a + 1, b - 1] == player && Field[a + 2, b - 2] == player && Field[a + 3, b - 3] == player)
+                {
+                    Winn = true;
+                }
+            }
+        }
+        // Count the win
+        if (Winn)
+        {
+            if (player == 1)
+            {
+                Player_win1++;
+            }
+            else if (player == 2)
+            {
+                Player_win2++;
+            }
+            Console.WriteLine("Spieler {0} Gewinnt", player);
+            Winn = false;
+            Console.ReadLine();
+            RenewField();
+        }
+    }
+
     private static int SwitchPlayerAndFiedback()
     {
         if (Player == 1)
